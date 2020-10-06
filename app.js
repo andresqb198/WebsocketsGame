36df28ff0588b715abae18cc5094d67dc4891d63
andresqb198 <andresqb198@gmail.com>
2020-10-05 23:44:49 -0500

    Game almost finished T.P.17

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-var mongojs = require("mongojs");
-var db = mongojs('localhost:27017/myGame', ['account','progress']);
- 
+//var mongojs = require("mongojs");
+var db = null;//mongojs('localhost:27017/myGame', ['account','progress']);
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
- 
+
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
- 
-serv.listen(2000);
+
+serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
- 
+
 var SOCKET_LIST = {};
- 
-var Entity = function(){
+
+var Entity = function(param){
 	var self = {
 		x:250,
 		y:250,
 		spdX:0,
 		spdY:0,
 		id:"",
+		map:'forest',
 	}
+	if(param){
+		if(param.x)
+			self.x = param.x;
+		if(param.y)
+			self.y = param.y;
+		if(param.map)
+			self.map = param.map;
+		if(param.id)
+			self.id = param.id;		
+	}
+	
 	self.update = function(){
 		self.updatePosition();
 	}
@@ -35,11 +47,11 @@ var Entity = function(){
 	}
 	return self;
 }
- 
-var Player = function(id){
-	var self = Entity();
-	self.id = id;
+
+var Player = function(param){
+	var self = Entity(param);
 	self.number = "" + Math.floor(10 * Math.random());
+	self.username = param.username;
 	self.pressingRight = false;
 	self.pressingLeft = false;
 	self.pressingUp = false;
@@ -47,22 +59,30 @@ var Player = function(id){
 	self.pressingAttack = false;
 	self.mouseAngle = 0;
 	self.maxSpd = 10;
- 
+	self.hp = 10;
+	self.hpMax = 10;
+	self.score = 0;
+	
 	var super_update = self.update;
 	self.update = function(){
 		self.updateSpd();
+		
 		super_update();
- 
+		
 		if(self.pressingAttack){
 			self.shootBullet(self.mouseAngle);
 		}
 	}
 	self.shootBullet = function(angle){
-		var b = Bullet(self.id,angle);
-		b.x = self.x;
-		b.y = self.y;
+		Bullet({
+			parent:self.id,
+			angle:angle,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		});
 	}
- 
+	
 	self.updateSpd = function(){
 		if(self.pressingRight)
 			self.spdX = self.maxSpd;
@@ -70,7 +90,7 @@ var Player = function(id){
 			self.spdX = -self.maxSpd;
 		else
 			self.spdX = 0;
- 
+		
 		if(self.pressingUp)
 			self.spdY = -self.maxSpd;
 		else if(self.pressingDown)
@@ -78,19 +98,45 @@ var Player = function(id){
 		else
 			self.spdY = 0;		
 	}
-	Player.list[id] = self;
- 
-	initPack.player.push({
-		id:self.id,
-		x:self.x,
-		y:self.y,	
-		number:self.number,	
-	});
+	
+	self.getInitPack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,	
+			number:self.number,	
+			hp:self.hp,
+			hpMax:self.hpMax,
+			score:self.score,
+			map:self.map,
+		};		
+	}
+	self.getUpdatePack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			hp:self.hp,
+			score:self.score,
+			map:self.map,
+		}	
+	}
+	
+	Player.list[self.id] = self;
+	
+	initPack.player.push(self.getInitPack());
 	return self;
 }
 Player.list = {};
-Player.onConnect = function(socket){
-	var player = Player(socket.id);
+Player.onConnect = function(socket,username){
+	var map = 'forest';
+	if(Math.random() < 0.5)
+		map = 'field';
+	var player = Player({
+		username:username,
+		id:socket.id,
+		map:map,
+	});
 	socket.on('keyPress',function(data){
 		if(data.inputId === 'left')
 			player.pressingLeft = data.state;
@@ -105,7 +151,45 @@ Player.onConnect = function(socket){
 		else if(data.inputId === 'mouseAngle')
 			player.mouseAngle = data.state;
 	});
+	
+	socket.on('changeMap',function(data){
+		if(player.map === 'field')
+			player.map = 'forest';
+		else
+			player.map = 'field';
+	});
+	
+	socket.on('sendMsgToServer',function(data){
+		for(var i in SOCKET_LIST){
+			SOCKET_LIST[i].emit('addToChat',player.username + ': ' + data);
+		}
+	});
+	socket.on('sendPmToServer',function(data){ //data:{username,message}
+		var recipientSocket = null;
+		for(var i in Player.list)
+			if(Player.list[i].username === data.username)
+				recipientSocket = SOCKET_LIST[i];
+		if(recipientSocket === null){
+			socket.emit('addToChat','The player ' + data.username + ' is not online.');
+		} else {
+			recipientSocket.emit('addToChat','From ' + player.username + ':' + data.message);
+			socket.emit('addToChat','To ' + data.username + ':' + data.message);
+		}
+	});
+	
+	socket.emit('init',{
+		selfId:socket.id,
+		player:Player.getAllInitPack(),
+		bullet:Bullet.getAllInitPack(),
+	})
 }
+Player.getAllInitPack = function(){
+	var players = [];
+	for(var i in Player.list)
+		players.push(Player.list[i].getInitPack());
+	return players;
+}
+
 Player.onDisconnect = function(socket){
 	delete Player.list[socket.id];
 	removePack.player.push(socket.id);
@@ -115,22 +199,20 @@ Player.update = function(){
 	for(var i in Player.list){
 		var player = Player.list[i];
 		player.update();
-		pack.push({
-			id:player.id,
-			x:player.x,
-			y:player.y,
-		});		
+		pack.push(player.getUpdatePack());		
 	}
 	return pack;
 }
- 
- 
-var Bullet = function(parent,angle){
-	var self = Entity();
+
+
+var Bullet = function(param){
+	var self = Entity(param);
 	self.id = Math.random();
-	self.spdX = Math.cos(angle/180*Math.PI) * 10;
-	self.spdY = Math.sin(angle/180*Math.PI) * 10;
-	self.parent = parent;
+	self.angle = param.angle;
+	self.spdX = Math.cos(param.angle/180*Math.PI) * 10;
+	self.spdY = Math.sin(param.angle/180*Math.PI) * 10;
+	self.parent = param.parent;
+	
 	self.timer = 0;
 	self.toRemove = false;
 	var super_update = self.update;
@@ -138,25 +220,46 @@ var Bullet = function(parent,angle){
 		if(self.timer++ > 100)
 			self.toRemove = true;
 		super_update();
- 
+		
 		for(var i in Player.list){
 			var p = Player.list[i];
-			if(self.getDistance(p) < 32 && self.parent !== p.id){
-				//handle collision. ex: hp--;
+			if(self.map === p.map && self.getDistance(p) < 32 && self.parent !== p.id){
+				p.hp -= 1;
+								
+				if(p.hp <= 0){
+					var shooter = Player.list[self.parent];
+					if(shooter)
+						shooter.score += 1;
+					p.hp = p.hpMax;
+					p.x = Math.random() * 500;
+					p.y = Math.random() * 500;					
+				}
 				self.toRemove = true;
 			}
 		}
 	}
+	self.getInitPack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		};
+	}
+	self.getUpdatePack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,		
+		};
+	}
+	
 	Bullet.list[self.id] = self;
-	initPack.bullet.push({
-		id:self.id,
-		x:self.x,
-		y:self.y,		
-	});
+	initPack.bullet.push(self.getInitPack());
 	return self;
 }
 Bullet.list = {};
- 
+
 Bullet.update = function(){
 	var pack = [];
 	for(var i in Bullet.list){
@@ -166,48 +269,54 @@ Bullet.update = function(){
 			delete Bullet.list[i];
 			removePack.bullet.push(bullet.id);
 		} else
-			pack.push({
-				id:bullet.id,
-				x:bullet.x,
-				y:bullet.y,
-			});		
+			pack.push(bullet.getUpdatePack());		
 	}
 	return pack;
 }
- 
+
+Bullet.getAllInitPack = function(){
+	var bullets = [];
+	for(var i in Bullet.list)
+		bullets.push(Bullet.list[i].getInitPack());
+	return bullets;
+}
+
 var DEBUG = true;
- 
+
 var isValidPassword = function(data,cb){
-	db.account.find({username:data.username,password:data.password},function(err,res){
+	return cb(true);
+	/*db.account.find({username:data.username,password:data.password},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
-	});
+	});*/
 }
 var isUsernameTaken = function(data,cb){
-	db.account.find({username:data.username},function(err,res){
+	return cb(false);
+	/*db.account.find({username:data.username},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
-	});
+	});*/
 }
 var addUser = function(data,cb){
-	db.account.insert({username:data.username,password:data.password},function(err){
+	return cb();
+	/*db.account.insert({username:data.username,password:data.password},function(err){
 		cb();
-	});
+	});*/
 }
- 
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
- 
-	socket.on('signIn',function(data){
+	
+	socket.on('signIn',function(data){ //{username,password}
 		isValidPassword(data,function(res){
 			if(res){
-				Player.onConnect(socket);
+				Player.onConnect(socket,data.username);
 				socket.emit('signInResponse',{success:true});
 			} else {
 				socket.emit('signInResponse',{success:false});			
@@ -225,40 +334,34 @@ io.sockets.on('connection', function(socket){
 			}
 		});		
 	});
- 
- 
+	
+	
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
 		Player.onDisconnect(socket);
 	});
-	socket.on('sendMsgToServer',function(data){
-		var playerName = ("" + socket.id).slice(2,7);
-		for(var i in SOCKET_LIST){
-			SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
-		}
-	});
- 
+	
 	socket.on('evalServer',function(data){
 		if(!DEBUG)
 			return;
 		var res = eval(data);
 		socket.emit('evalAnswer',res);		
 	});
- 
- 
- 
+	
+	
+	
 });
- 
+
 var initPack = {player:[],bullet:[]};
 var removePack = {player:[],bullet:[]};
- 
- 
+
+
 setInterval(function(){
 	var pack = {
 		player:Player.update(),
 		bullet:Bullet.update(),
 	}
- 
+	
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
 		socket.emit('init',initPack);
@@ -269,5 +372,30 @@ setInterval(function(){
 	initPack.bullet = [];
 	removePack.player = [];
 	removePack.bullet = [];
- 
+	
 },1000/25);
+
+/*
+var profiler = require('v8-profiler');
+var fs = require('fs');
+var startProfiling = function(duration){
+	profiler.startProfiling('1', true);
+	setTimeout(function(){
+		var profile1 = profiler.stopProfiling('1');
+		
+		profile1.export(function(error, result) {
+			fs.writeFile('./profile.cpuprofile', result);
+			profile1.delete();
+			console.log("Profile saved.");
+		});
+	},duration);	
+}
+startProfiling(10000);
+*/
+
+
+
+
+
+
+
